e412c1e51c5fb0fe6a0e426d6eac4dfb0dfb9009
Alix Grillo <alix.grillo2+github@gmail.com>
2019-11-03 13:49:54 -0700

    completed logic to display gifs of clicked animal

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,24 +1,22 @@
-console.log("hi");
-
+// initial list of buttons to display on page
 var animals = ["dog", "cat", "bird"];
-
 
 // Function for displaying animal buttons
 function createButtons() {
 
-    // Deleting the movie buttons prior to adding new movie buttons
+    // Clears the buttons that are on page, to repopulate them
     // (this is necessary otherwise we will have repeat buttons)
     $("#buttonsDiv").empty();
 
-    // Looping through the array of movies
+    // Looping through the array of animals
     for (var i = 0; i < animals.length; i++) {
 
-        // Then dynamicaly generating buttons for each movie in the array.
-        // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+        // Then dynamicaly generating buttons for each animal in the array
         var a = $("<button>");
-        // Adding a class
-        a.addClass("animal-button");
-        // Adding a data-attribute with a value of the movie at index i
+        // Adding a class to tie to animal button
+        a.addClass("animal-button btn btn-dark m-2");
+        // Adding a data-attribute with a value of the animal name at index i
+        // this will be used for the giphy query
         a.attr("animal", animals[i]);
         // Providing the button's text with a value of the movie at index i
         a.text(animals[i]);
@@ -29,34 +27,63 @@ function createButtons() {
 
 createButtons();
 
-      // This function handles events where one button is clicked
-      $("#add-animal").on("click", function(event) {
-        // event.preventDefault() prevents the form from trying to submit itself.
-        // We're using a form so that the user can hit enter instead of clicking the button if they want
-        event.preventDefault();
+// This function handles events where one button is clicked
+$("#add-animal").on("click", function(event) {
+    // event.preventDefault() prevents the form from trying to submit itself.
+    // We're using a form so that the user can hit enter instead of clicking the button if they want
+    event.preventDefault();
 
-        // This line will grab the text from the input box
-        var animal = $("#animal-input").val().trim();
-        // The movie from the textbox is then added to our array
-        animals.push(animal);
+    // This line will grab the text from the input box
+    var animal = $("#animal-input").val().trim();
+    // The movie from the textbox is then added to our array
+    animals.push(animal);
 
-        // calling renderButtons which handles the processing of our movie array
-        createButtons();
-      });
+    // calling renderButtons which handles the processing of our movie array
+    createButtons();
+});
 
-$(".animal-button").on("click", function(){
+$(document).on("click", ".animal-button", populateGifs)
+
+function populateGifs(){
+    $("#gifDiv").empty();
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?";
-    //var queryURL = "https://api.giphy.com/v1/gifs/random?";
+
     var apiKey = "api_key=vo4Gg8GsJD8WqzEg7o18BpxDgzmio0V5";
 
-    var q = "";
-    var limit = 10;
+    var animal = $(this).attr("animal");
 
     $.ajax({
-        url: queryURL + apiKey + "&&tag=" + $(this).attr("animal"),
+        url: queryURL + apiKey + "&&limit=10&&q=" + animal,
         method: "GET"
     }).then(function(response){
-        console.log(response);
+        var results = response.data;
+        $.each(results, function(i, gif){
+            var imgDiv = $("<div>");
+            imgDiv.append("<p>Rating: " + results[i].rating);
+            var img = $("<img>");
+            img.attr("src", results[i].images.fixed_height_still.url);
+            img.attr("alt", results[i].title);
+            img.attr("img-still", results[i].images.fixed_height_still.url);
+            img.attr("img-animate", results[i].images.fixed_height.url);
+            img.attr("state", "still");
+            img.addClass("gif-img");
+            imgDiv.append(img);
+            $("#gifDiv").append(imgDiv);  
+        })
     })
-})
+}
+
+$(document).on("click", ".gif-img", animateGifs)
+
+function animateGifs(){
+    var state = $(this).attr("state");
+    if(state==="still"){
+        $(this).attr("src",$(this).attr("img-animate"));
+        $(this).attr("state","animate");
+    } else {
+        $(this).attr("src",$(this).attr("img-still"));
+        $(this).attr("state","still");
+    }
+}
+
